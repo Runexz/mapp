eeeb42f5c2ccc560ab59fe9fe78ff74b4d106606
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-11 15:05:13 -0600

    imported Home, About, Services with routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import Navbar from './components/Navbar';
+import {Route} from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Error from './components/pages/Error';
+import Services from './components/pages/Services';
+
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      Hi from App.js!
       <Navbar />
+
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/services" component={Services} />
     </React.Fragment>
   );
 }
