f7c6ddcecf7c167f5adde079412b233017f72bc7
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 17:41:45 -0600

    added const Ordinance

--- src/components/ordinance/Ordinance.js
+++ src/components/ordinance/Ordinance.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import './Ordinance.css'
+
+const Ordinance = () => {
+    return (
+        <Container className='ordinanceOne p-4 text-center m-2'>
+            <h3>1 of 2</h3>
+        </Container>
+    );
+}
+
+export default Ordinance;
