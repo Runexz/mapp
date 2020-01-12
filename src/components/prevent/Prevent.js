63786d5c49685cbf1064a65085ff96170da30491
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 17:44:59 -0600

    created Prevent container

---
+++ src/components/prevent/Prevent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import './Prevent.css'
+
+const Prevent = () => {
+    return (
+        <Container className='preventOne p-4 text-center m-2'>
+            <h3>2 of 2</h3>
+        </Container>
+    );
+}
+
+export default Prevent;
