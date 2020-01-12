f8366e20f9d157c977eff10e0c1dff26a9976da4
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 17:34:15 -0600

    added Apply component

--- src/components/apply/Apply.js
+++ src/components/apply/Apply.js
@@ -1,2 +1,14 @@
 import React from 'react';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import './Apply.css'
+
+const Apply = () => {
+    return (
+        <Container className='applyOne p-4 text-center m-2'>
+            <h3>1 of 2</h3>
+        </Container>
+    );
+}
+
+export default Apply;
