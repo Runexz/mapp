7231fe90d5d87aa6e8329868d44e5407e8702041
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 17:34:38 -0600

    added Signin component and css

---
+++ src/components/signIn/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import './Signin.css';
+
+const Signin = () => {
+    return (
+        <Container className='signinOne p-4 m-2 text-center'>
+            <h3>2 of 2</h3>
+        </Container>
+    );
+}
+
+export default Signin;
