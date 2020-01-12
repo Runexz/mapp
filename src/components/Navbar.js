37a76416993fd80f5347610d93e9fb39a85fd6b4
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-11 19:10:56 -0600

    created images folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
 import React from 'react';
 
+import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const NavbarHeading = () => {
     return(
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/services">Services</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="/">City Alarm</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/services">Services</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+        // <nav>
+        //     <ul>
+        //         <li>
+        //             <NavLink to="/">Home</NavLink>
+        //         </li>
+        //         <li>
+        //             <NavLink to="/services">Services</NavLink>
+        //         </li>
+        //         <li>
+        //             <NavLink to="/about">About</NavLink>
+        //         </li>
+        //     </ul>
+        // </nav>
     )
 
 };
 
-export default Navbar;
+export default NavbarHeading;
