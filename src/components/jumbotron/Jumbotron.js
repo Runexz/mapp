21063fc379dcb276e3dddb227717ddf70f592e00
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 16:36:00 -0600

    created jumbotronHeading

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+import { Jumbotron, Container } from 'react-bootstrap';
+
+import './Jumbotron.css'
+
+const JumbotronHeading = () => {
+    return (
+        <Jumbotron className="jumboPic mt-5 text-white">
+            <Container className="pt-5 pb-5">
+                <h1>Mesquite Alarm Permit Program</h1>
+            </Container>
+        </Jumbotron>
+    );
+}
+
+export default JumbotronHeading;
