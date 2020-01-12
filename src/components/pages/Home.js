10d997c7ebff9fa94c9d7f0212946a917e266666
Ricardo Rodriguez <m3ssiak@gmail.com>
2020-01-12 17:36:27 -0600

    added Apply and Signin with media queries

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import JumbotronHeading from '../jumbotron/Jumbotron';
 import { Container, Row, Col } from 'react-bootstrap';
+import Apply from '../apply/Apply';
+import Signin from '../signIn/Signin';
 
 const Home = () => {
     return (
         <div>
-        <JumbotronHeading />
-        <Container>
-            <Row>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
-            </Row>
-        </Container>
+            <JumbotronHeading />
+            <Container>
+                <Row>
+                    <Col sm={12} md={6} lg={6}><Apply /></Col>
+                    <Col sm={12} md={6} lg={6}><Signin /></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
